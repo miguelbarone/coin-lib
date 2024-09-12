3836a6d06ecff483053f035c556c1201387d297e
miguelbarone <miguel.barone@picpay.com>
2024-09-12 19:28:01 -0300

    organizing project folders

--- coin-lib/CoinLib/AppConfiguration/AppDelegate.swift
+++ coin-lib/CoinLib/AppConfiguration/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow()
-        let navigationController = UINavigationController(rootViewController: ViewController())
+        let navigationController = UINavigationController(rootViewController: UIViewController())
 
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
